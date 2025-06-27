a6ff8f77a59b2493cabc698ec019c29daf55c210
Gustavo-Morchel <gustavomorchel@gmail.com>
2025-06-26 21:43:25 -0300

    Teste de Forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1 className="text-3xl font-bold underline bg-blue-700 p-5 border-8 border-cyan-400 md:bg-red-700">
+        Hello world!
+      </h1>
+      <button className="uppercase rounded-4xl w-80 bg-green-700  md:bg-red-500 hover:bg-amber-400 cursor-pointer p-5 font-bold text-white text-4xl ">
+        Me clique
+      </button>
+      <div className="">
+        <form action="https://getform.io/f/byvyjzma" method="POST">
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <input type="text" name="message" />
+        {/* add hidden Honeypot input to prevent spams */}
+        <input
+          type="hidden"
+          name="_gotcha"
+          style={{ display: "none !important" }}
+        />
+        {/* checkbox handle */}
+        <input
+          type="checkbox"
+          name="subscribe"
+          defaultValue="yes"
+          defaultChecked=""
+        />
+        <input type="hidden" name="subscribe" defaultValue="no" />
+        {/* radio button handle */}
+        <input
+          type="radio"
+          name="gender"
+          defaultValue="male"
+          defaultChecked=""
+        />
+        <input type="radio" name="gender" defaultValue="female" />
+        <input type="radio" name="gender" defaultValue="other" />
+        {/* select field handle */}
+        <select name="work-experience">
+          <option value="one-year">0-1 years</option>
+          <option value="one-five-years">1-5 years</option>
+        </select>
+        <button type="submit">Send</button>
+      </form>
       </div>
-      <h1>Teste</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
